c90d39c788b987ee0a2bda240902c0fd93c37ff9
Albhieri <alcrivico@gmail.com>
2023-11-25 14:45:31 -0600

    Se modularizó el CSS del FXMLInicioSesion y se agregó una plantilla FXML de ejemplo para nuevas interfaces

--- src/Sgbp.java
+++ src/Sgbp.java
@@ -1,9 +1,4 @@
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -17,22 +12,12 @@ public class Sgbp extends Application {
     @Override
     public void start(Stage escenario) throws Exception {
 
-        try {
+        Font.loadFont(Utilidades.getInputStream("/recursos/fuentes/Poppins-Regular.ttf"), 12);
 
-            Parent vista = FXMLLoader.load(Utilidades.getURL("/vista/FXMLInicioSesion.fxml"));
-            Scene escena = new Scene(vista);
-            String css = Utilidades.getURLString("/vista/estilos/FXMLInicioSesion.css");
-
-            Font.loadFont(Utilidades.getInputStream("/recursos/fuentes/Poppins-Regular.ttf"), 12);
-            escena.getStylesheets().add(css);
-            escenario.setScene(escena);
-            escenario.setTitle("Sistema Gestor de Bitácoras de Proyecto");
-            escenario.setResizable(false);
-            escenario.show();
-
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
+        Utilidades.inicializarVentana(escenario,
+                "/vista/FXMLInicioSesion.fxml",
+                "/vista/estilos/escenaInicioSesion.css",
+                "Sistema Gestor de Bitácoras de Proyecto", false);
 
     }
 
