177b271225c0d4a846f47c489be36e92454f18b3
Albhieri <alcrivico@gmail.com>
2023-11-25 23:19:19 -0600

    Se cargaron mas estilos de fuentes, y se agregó hover a los paneles de menu

--- src/controlador/FXMLRegistrarActividadController.java
+++ src/controlador/FXMLRegistrarActividadController.java
@@ -2,7 +2,6 @@ package controlador;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
