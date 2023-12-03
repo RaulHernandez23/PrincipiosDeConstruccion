142877e8c3f02023d9c7786e2b7453805e882f56
RaulHernandez23 <raulh230600@gmail.com>
2023-12-03 11:52:55 -0600

    Coreccion de pequeños errores en AsignarValoresEjemplo

--- bd/AsignarValoresEjemplo.sql
+++ bd/AsignarValoresEjemplo.sql
@@ -27,13 +27,13 @@ INSERT INTO tipoActividad (tipo) VALUES
 ('Controladores'),
 ('Otro Archivo');
 
-INSERT INTO idEstadoProyecto (estado) VALUES
+INSERT INTO estadoProyecto (estado) VALUES
 ('Activo'),
 ('Finalizado');
 
 INSERT INTO estadoEstudiante (estado) VALUES
 ('Activo'),
-('Inactivo')
+('Inactivo');
 
 INSERT INTO periodoEscolar (nombre, fechaInicio, fechaFin) VALUES
 ('Febrero-Julio 23', '2023-02-01', '2023-07-31'),
@@ -44,7 +44,7 @@ INSERT INTO proyecto (nombre, descripcion, idEstadoProyecto) VALUES
 ('Gastrocafé', 'Proyecto Gastrocafé pedido por el café para su software de gestión', 1);
 
 -- Relacionar proyecto con periodo escolar
-INSERT INTO periodoEscolar_Proyecto (idPeriodoEscolar, idProyecto) VALUES
+INSERT INTO proyecto_periodoEscolar (idPeriodoEscolar, idProyecto) VALUES
 (1, 1),
 (2, 1);
 
@@ -59,7 +59,7 @@ VALUES
 ('S21026432', 'Raul', 'Hernandez', 'Olivares', 'pass', 1, 1);
 
 -- Relacionar estudiante con periodoEscolar
-INSERT INTO periodoEscolar_Estudiante (idPeriodoEscolar, idEstudiante) VALUES
+INSERT INTO estudiante_periodoEscolar (idPeriodoEscolar, idEstudiante) VALUES
 (1, 2),
 (2, 2);
 
@@ -84,12 +84,12 @@ INSERT INTO defecto (titulo, descripcion, esfuerzoMinutos, fechaReporte, fechaFi
 ('Defecto 5', 'Descripción 5', 300, '2023-08-09', '2023-08-10', 2, 2, 1);
 
 -- Insertar solicitudes de cambio
-INSERT INTO solicitudCambio (titulo, descripcion, fechaReporte, fechaEvaluacion, idEstudiante, idEstadoSolicitud, idResponsableProyecto, idDefecto) VALUES
-('Solicitud 1', 'Descripción 1', '2023-08-01', '2023-08-02', 2, 1, 1, 1),
-('Solicitud 2', 'Descripción 2', '2023-08-03', '2023-08-04', 2, 2, 1, 2),
-('Solicitud 3', 'Descripción 3', '2023-08-05', '2023-08-06', 2, 1, 1, 3),
-('Solicitud 4', 'Descripción 4', '2023-08-07', '2023-08-08', 2, 2, 1, 4),
-('Solicitud 5', 'Descripción 5', '2023-08-09', '2023-08-10', 2, 3, 1, 5);
+INSERT INTO solicitudDeCambio (titulo, descripcion, fechaReporte, fechaEvaluacion, idEstudiante, idEstadoSolicitud, idProyecto, idResponsableProyecto, idDefecto) VALUES
+('Solicitud 1', 'Descripción 1', '2023-08-01', '2023-08-02', 2, 1, 1, 1, 1),
+('Solicitud 2', 'Descripción 2', '2023-08-03', '2023-08-04', 2, 2, 1, 1, 2),
+('Solicitud 3', 'Descripción 3', '2023-08-05', '2023-08-06', 2, 1, 1, 1, 3),
+('Solicitud 4', 'Descripción 4', '2023-08-07', '2023-08-08', 2, 2, 1, 1, 4),
+('Solicitud 5', 'Descripción 5', '2023-08-09', '2023-08-10', 2, 3, 1, 1, 5);
 
 -- Insertar cambios
 INSERT INTO cambio (titulo, descripcion, idEstadoCambio, fechaInicio, fechaFin, idSolicitudDeCambio) VALUES
